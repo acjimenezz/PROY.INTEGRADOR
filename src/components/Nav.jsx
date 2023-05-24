0432442fbd0982466f810d65d4c54fd9d57fc336
Andres Jimenez <114513945+acjimenezz@users.noreply.github.com>
2023-05-24 11:10:11 -0500

    hw10 react-forms (agg.logout)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,21 @@ import SearchBar from "./SearchBar";
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
 
+
+
+function Nav ({onSearch}){
+
+    return(
+        <ContainerDiv >
+            <Link to="/About"><Button>About</Button></Link>
+            <Link to="/home"><Button>Home</Button></Link>
+            <SearchBar onSearch={onSearch}/>
+            <Link to="/"><Button2>Logout</Button2></Link>            
+        </ContainerDiv>
+        
+    )
+};
+
 const ContainerDiv=styled.div`
     background-color: rgba(38, 38, 38, 0.5);
     display: flex;
@@ -25,18 +40,20 @@ const Button=styled.button`
     cursor: pointer;
   }
 `
+const Button2=styled.button`
+    background-color: purple ;
+    color: #ffffff;
+    border-radius: 10px;
+    padding: 5px;
+    font-family: Verdana;
+    font-weight: bold;
+    padding-left:15px;
+    padding-right:15px;
 
-
-function Nav ({onSearch}){
-
-    return(
-        <ContainerDiv >
-            <Link to="/About"><Button>About</Button></Link>
-            <Link to="/home"><Button>Home</Button></Link>
-            <SearchBar onSearch={onSearch}/>
-        </ContainerDiv>
-        
-    )
-};
+    &:hover {
+    transform: scale(1.1);
+    cursor: pointer;
+  }
+`
 
 export default Nav;
